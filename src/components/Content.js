5afd15d48923a08911f727e08d00f790dda3f869
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-10-09 08:18:47 +0530

    basic react styling learning

---
+++ src/components/Content.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './content.css'
+
+export default function Content() {
+  return (
+    <div className='paragraph'>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere explicabo recusandae fuga voluptatum cupiditate beatae vitae, est tempore itaque iste esse omnis, possimus eligendi? Nulla vero est dolorum maxime dignissimos.</p>
+    </div>
+  )
+}
+// mulnma js file ekk hadala itpasse yata file eke tipe eka react wlt maru krgnnw
+// rfc =====> rfc kiyn ek dila meke basic code tik gnnw
+// npm start =====> run krnne terminal eke npm start dila enter krnn on 
