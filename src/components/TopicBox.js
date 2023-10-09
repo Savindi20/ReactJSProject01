859bcef166ce76cb9462828611c1f77a50326102
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-10-09 07:58:37 +0530

    topicBox component file style added

--- src/components/TopicBox.js
+++ src/components/TopicBox.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import './topicbox.css' 
 
 export default function TopicBox() {
   return (
     <div>
       <div className='topicBox'>
-        <span>This is the topic</span>
+        <span className='text'>This is the topic</span>
       </div>
     </div>
   )
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,7 @@ import TopicBox from '../components/TopicBox'
 export default function Home() {
   return (
     <div>
-      <h1>This is the HomePage</h1>
+        <h1>Home Page</h1>
       <TopicBox/>
     </div>
   )
