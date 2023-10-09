35c7c630f5b1db6e6b693ec787539d8409c2b8f9
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-10-09 07:29:17 +0530

    component file added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import PartOne from './components/partOne';
 
 function App() {
   return (
     <div className="App">
       <h1>This is my first React Project</h1>
+      
+      <PartOne/>
     </div>
   );
 }
---
+++ src/components/partOne.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function partOne() {
+  return (
+    <div>
+      <h1>This is my first react component</h1>
+    </div>
+  )
+}
