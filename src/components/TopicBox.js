4d834e5b6647db38319f10cef039310291cf1bf4
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-10-16 22:27:25 +0530

    dianamic valiables display

--- src/components/TopicBox.js
+++ src/components/TopicBox.js
@@ -2,10 +2,13 @@ import React from 'react'
 import './topicbox.css' 
 
 export default function TopicBox() {
+  const topic = "My Name Savii"
+  const item = "Live in Matara"
+
   return (
     <div>
       <div className='topicBox'>
-        <span className='text'>This is the topic</span>
+        <span className='text'>{topic} {item}</span>
       </div>
     </div>
   )
