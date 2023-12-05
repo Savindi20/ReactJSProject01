bfe8e89313d69241ea25548b21f6f76b7c58ad07
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-12-05 22:02:49 +0530

    updated with react contents

--- src/components/TopicBox.js
+++ src/components/TopicBox.js
@@ -2,13 +2,19 @@ import React from 'react'
 import './topicbox.css' 
 
 export default function TopicBox() {
-  const topic = "My Name Savii"
-  const item = "Live in Matara"
+  // const topic = "My favourite food is Kottu"
+  // const item = "and fride rice"
+
+  // const a = 10;
+  // const b = 20;
+  // const c = a + b;
 
   return (
     <div>
       <div className='topicBox'>
-        <span className='text'>{topic} {item}</span>
+        {/* <span className='text'>{topic} {item}</span> */}
+        {/* <span className='text'>{c}</span> */}
+        <span className='text'>{alert("Hello")}</span>
       </div>
     </div>
   )
