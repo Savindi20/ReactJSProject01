a3c71ed7c9fa54835044ede27461a333321dbac2
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-12-05 21:16:49 +0530

    content file updated

--- src/components/Content.js
+++ src/components/Content.js
@@ -4,10 +4,13 @@ import './content.css'
 export default function Content() {
   return (
     <div className='paragraph'>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere explicabo recusandae fuga voluptatum cupiditate beatae vitae, est tempore itaque iste esse omnis, possimus eligendi? Nulla vero est dolorum maxime dignissimos.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere explicabo recusandae fuga voluptatum cupiditate beatae vitae, est tempore itaque iste esse omnis, possimus eligendi? Nulla vero est dolorum maxime dignissimos.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere explicabo recusandae fuga voluptatum cupiditate beatae vitae, est tempore itaque iste esse omnis, possimus eligendi? Nulla vero est dolorum maxime dignissimos.
+      </p>
     </div>
   )
 }
-// mulnma js file ekk hadala itpasse yata file eke tipe eka react wlt maru krgnnw
+//npx create react app <project name> =====> npx create react app ekak hadala react app eka hadagannawa
+// mulnma js file ekk hadala itpasse status bar eke yata file eke type eka react wlt maru krgnnw
 // rfc =====> rfc kiyn ek dila meke basic code tik gnnw
 // npm start =====> run krnne terminal eke npm start dila enter krnn on 
