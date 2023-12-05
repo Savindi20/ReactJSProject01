d6b944ec0ce3cbd7c0dbd5b9474ec54b3c0bcf1c
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-12-05 22:27:23 +0530

    return object value theory added

--- src/components/TopicBox.js
+++ src/components/TopicBox.js
@@ -9,12 +9,17 @@ export default function TopicBox() {
   // const b = 20;
   // const c = a + b;
 
+  // const foodprices = {kottu:"250",frideRice:"350",noodles:"240",riceAndCurry:"300"} mew kala hodaki
+  // me widiyt boolean value, object return krnn ba
+  //array,variable,string,number wage dewl return krnn puluwn
   return (
     <div>
       <div className='topicBox'>
         {/* <span className='text'>{topic} {item}</span> */}
         {/* <span className='text'>{c}</span> */}
-        <span className='text'>{alert("Hello")}</span>
+        {/* <span className='text'>{alert("Hello")}</span> */}
+        {/* <span className='text'>{foodprices}</span> meka waradi object return */}
+        <span className='text'>My favourite food is Kottu and fride rice</span>
       </div>
     </div>
   )
