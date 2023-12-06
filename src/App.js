b18766a79c49d6879d35a91e131290e166a42098
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-12-06 15:10:07 +0530

    counter added

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { BrowserRouter as Route} from 'react-router-dom';
-import { BrowserRouter as Switch} from 'react-router-dom';
+import './App.css';
+// import { BrowserRouter as Router } from 'react-router-dom';
+// import { BrowserRouter as Route} from 'react-router-dom';
+// import { BrowserRouter as Switch} from 'react-router-dom';
 
-import Home from './pages/Home';
-import About from './pages/About';
-import Contact from './pages/Contact';
+// import Home from './pages/Home';
+// import About from './pages/About';
+// import Contact from './pages/Contact';
 
 function App() {
+  // let [count, setCount] = React.useState(0);
+  let count = 0;
   return (
-    <Router>
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/about" component={About} />
-        <Route path="/contact" component={Contact} />
-      </Switch>
-    </Router>
+    <>
+      <span className='title'>My Counter</span>
+      <p className='subTitle'>The Count is {count}</p>
+      <button className='button'>+</button>
+      <button className='button'>-</button>
+      {/* <button onClick={() => setCount(count + 1)}>Add</button>
+      <button onClick={() => setCount(count - 1)}>Subtract</button> */}
+    </>
+    
+    // <Router>
+    //   <Switch>
+    //     <Route path="/" exact component={Home} />
+    //     <Route path="/about" component={About} />
+    //     <Route path="/contact" component={Contact} />
+    //   </Switch>
+    // </Router>
   );
 }
 
