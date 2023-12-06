119891510030f7cd7ccb4ce38474db5c2ac3815f
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-12-06 15:44:07 +0530

    react bootstrap added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
-import { useState } from 'react';
+import Accordion from 'react-bootstrap/Accordion';
+// import { useState } from 'react';
+
 // import { BrowserRouter as Router } from 'react-router-dom';
 // import { BrowserRouter as Route} from 'react-router-dom';
 // import { BrowserRouter as Switch} from 'react-router-dom';
@@ -10,29 +12,61 @@ import { useState } from 'react';
 // import Contact from './pages/Contact';
 
 function App() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
 
-  // let count = 0;
-  const increment = () => {
-    // count++;
-    setCount(count + 1);
-  }
+  // // let count = 0;
+  // const increment = () => {
+  //   // count++;
+  //   setCount(count + 1);
+  // }
 
-  const decrement = () => { 
-    // count--;
-    setCount(count - 1);
-  }
+  // const decrement = () => { 
+  //   // count--;
+  //   setCount(count - 1);
+  // }
 
   return (
-    <>
-      <span className='title'>My Counter</span>
+    <div>
+      <h1>React bootstrap</h1>
+      <div className="acordin">
+        <Accordion defaultActiveKey="0">
+          <Accordion.Item eventKey="0">
+            <Accordion.Header>Accordion Item #1</Accordion.Header>
+            <Accordion.Body>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+              minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+              aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+              pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+              culpa qui officia deserunt mollit anim id est laborum.
+            </Accordion.Body>
+          </Accordion.Item>
+          <Accordion.Item eventKey="1">
+            <Accordion.Header>Accordion Item #2</Accordion.Header>
+            <Accordion.Body>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+              minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+              aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+              pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+              culpa qui officia deserunt mollit anim id est laborum.
+            </Accordion.Body>
+          </Accordion.Item>
+        </Accordion>
+      </div>
+      
+      {/* <span className='title'>My Counter</span>
       <p className='subTitle'>The Count is {count}</p>
       <button onClick={increment} className='button'>+</button>
-      <button onClick={decrement} className='button'>-</button>
-      
+      <button onClick={decrement} className='button'>-</button> */}
+
       {/* <button onClick={() => setCount(count + 1)}>Add</button>
       <button onClick={() => setCount(count - 1)}>Subtract</button> */}
-    </>
+
+      {/* use state ekk use krl asign krn value ekk  percistant, constant value ekk nemei */}
+    </div>
     
     // <Router>
     //   <Switch>
