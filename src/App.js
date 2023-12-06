22d69e2ccdae45b4cc0041c408f9962f81f37aa0
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-12-06 14:47:20 +0530

    react router added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Route} from 'react-router-dom';
+import { BrowserRouter as Switch} from 'react-router-dom';
+
 import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
 
 function App() {
   return (
-    <div className="App">
-    <Home/>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
+      </Switch>
+    </Router>
   );
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,20 +1,12 @@
 import React from 'react'
-import TopicBox from '../components/TopicBox'
-import Content from '../components/Content'
+import { Link } from 'react-router-dom'
 
 export default function Home() {
   return (
     <div>
       <h1>Home Page</h1>
-      {/* <TopicBox food = "Kottu" price="RS 350"> 
-        <span>This is the description</span>
-      </TopicBox>
-      <TopicBox food = "Rice" price="RS 250">
-        <button>This is a button</button>
-      </TopicBox>
-      <TopicBox food = "Hoppers" price="RS 150">
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis in corrupti quasi nam. Natus non esse quae aperiam blanditiis suscipit similique porro, inventore distinctio ipsa repellendus magnam labore molestias delectus!</p>
-      </TopicBox> */}
+      <Link to="/about">About</Link>
+
     </div>
   )
 }
